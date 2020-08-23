1e83901b58084c81f2644c89c8fcbd3683298d6b
tuananhpv0210 <tuananhpv0210@gmail.com>
2020-08-24 01:08:35 +0700

    update new database

--- database/demo.sql
+++ database/demo.sql
@@ -1,20 +1,24 @@
--- xóa cơ sở dữ liệu 
-DROP DATABASE demophp;
--- tạo cơ sở dữ liệu 
+/*
+Author: tuananhpv0210
+Updated_at: 00:42 24/08/2020
+*/
+
+-- Drop database
+DROP DATABASE IF EXISTS demophp;
 CREATE DATABASE demophp CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 USE demophp;
 
-
--- tạo bảng danh mục với product 
-
-CREATE TABLE tbl_user(
+-- Create users
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
 	id int PRIMARY KEY AUTO_INCREMENT,
 	name varchar(50) not null,
-	email varchar(50) UNIQUE,
-	username varchar(50) UNIQUE,
-	password varchar(255) not null,
-	birthday varchar(10),
-	address varchar(100),
-	created_at TIMESTAMP NOT NULL,
-	updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE now()
+	email varchar(30) UNIQUE,
+	username varchar(30) UNIQUE,
+	password varchar(200) not null,
+	birthday date,
+	address varchar(30),
+	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  	updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE now()
+ );
